9818eec833e16afa2a1394c70ade9130af71f4eb
phil cowan <philipjcowan1@gmail.com>
2020-08-05 18:45:12 -0700

    a few more things

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,23 +1,26 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if(Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto)
+        }else{
         return this.tasksService.getAllTasks();
+        }
     }
     
     @Get('/:id')
     getTaskById(@Param('id') id: string): Task {
         return this.tasksService.getTaskById(id)
-
     }
-
 
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task{
